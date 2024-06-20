f5ffe7ab6f4bf5a32d2e361a9b0d08921f211c2f
kimkimikk <165643812+kimkimikk@users.noreply.github.com>
2024-06-20 22:33:13 +0900

    ActivatePy 주석 추가

--- src/ActivatePy.java
+++ src/ActivatePy.java
@@ -8,8 +8,8 @@ public class ActivatePy {
     public static void start() {
         try {
             String virtualPath = "가상 환경 경로/python.exe"; //가상 환경 경로 설정
-            ProcessBuilder builder = new ProcessBuilder(virtualPath, "eye_tracker.py 경로"); //eye_tracker.py 경로 설정
-            //builder.directory(new File("C:/python/pythonProject/adhd"));
+            ProcessBuilder builder = new ProcessBuilder(virtualPath, "eye_tracker.py 경로/eye_tracker.py"); //eye_tracker.py 경로 설정
+            //builder.directory(new File("eye_tracker.py 경로")); -> 주석 처리 해제 시 eye_tracker.py가 속한 폴더에 log.csv 파일 생성
             process = builder.start();
             System.out.println("Process is started.");
         } catch (IOException e) {
